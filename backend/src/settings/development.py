0082648a2d202bfe4d97f2d1afacd59aedc2d722
burtrico <burtrico@gmail.com>
2023-09-08 13:14:38 -0500

    added example .env file

--- backend/src/settings/development.py
+++ backend/src/settings/development.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
@@ -20,10 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0k$l)lsg6$-5n=f&^8qq*io8*efy^#9%nt1xa@b(zv0+6#-k1b'
+SECRET_KEY = config('SECRET_KEY')
+
+YAHOO_APP_ID = config('YAHOO_APP_ID')
+YAHOO_CLIENT_ID = config('YAHOO_CLIENT_ID')
+YAHOO_CLIENT_SECRET = config('YAHOO_CLIENT_SECRET')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
